6df90b304907e783b5c2717341d5e9fa7f279afd
Darj0 <baranovadar04@gmail.com>
2024-09-22 14:24:17 +0300

    main.cpp
    Prideta duomenu ivedimo funkcija

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+void duomenys(vector<Studentas>& studentai);
 
 struct Studentas {
     string vardas;
@@ -12,5 +13,89 @@ struct Studentas {
 int main()
 {
   vector<Studentas> studentai;
+  duomenys(studentai);
   return 0;
 }
+//--------------------------------------------
+void duomenys(vector<Studentas>& studentai)
+{
+
+int studentuSkaicius;
+
+    while (true) {
+        cout << "Kiek studentu norite ivesti? \n";
+        cin >> studentuSkaicius;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Klaidingas ivedimas, bandykite dar karta (Iveskite tik skaiciu): \n";
+        } else {
+            break;
+        }
+    }
+
+
+    for (int i = 0; i < studentuSkaicius; i++) {
+        Studentas naujasStudentas;
+        cout << "Iveskite studento varda: ";
+        cin >> naujasStudentas.vardas;
+        naujasStudentas.vardas = capitalize(naujasStudentas.vardas);
+        cout << "Iveskite studento pavarde: ";
+        cin >> naujasStudentas.pavarde;
+        naujasStudentas.pavarde = capitalize(naujasStudentas.pavarde);
+
+        
+        int nd_count;
+        while (true) {
+            cout << "Kiek namu darbu ivertinimu norite ivesti? \n";
+            cin >> nd_count;
+
+            if (cin.fail())
+            {
+                cin.clear();
+                cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                cout << "Klaidingas ivedimas, bandykite dar karta (Iveskite tik skaiciu): \n";
+            }
+            else
+            {
+                cout << "Iveskite namu darbu ivertinimus (atskiriant ENTER): \n";
+                for (int j = 0; j < nd_count; j++)
+                {
+                    int nd_verte;
+
+                    while (true)
+                    {
+                        cin >> nd_verte;
+
+                        if (cin.fail()) {
+                            cin.clear();
+                            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                            cout << "Klaida! Iveskite skaiciu nuo 1 iki 10: \n";
+                        } else if (nd_verte < 1 || nd_verte > 10) {
+                            cout << "Klaida! Iveskite skaiciu nuo 1 iki 10: \n";
+                        } else {
+                            naujasStudentas.nd.push_back(nd_verte);
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+        }
+    
+            cout << "Iveskite egzamino ivertinima: ";
+            while (true)
+                    {
+                        cin >> naujasStudentas.egz;
+
+                        if (cin.fail()) {
+                            cin.clear();
+                            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                            cout << "Klaida! Iveskite skaiciu nuo 1 iki 10: \n";
+                        } else if (naujasStudentas.egz < 1 || naujasStudentas.egz > 10) {
+                            cout << "Klaida! Iveskite skaiciu nuo 1 iki 10: \n";
+                        } else {
+                            naujasStudentas.nd.push_back(naujasStudentas.egz);
+                            break;
+                        }
+                    }
