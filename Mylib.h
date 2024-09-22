79bd1269b2f897693ec034efe38a74df5d755ee4
Darj0 <baranovadar04@gmail.com>
2024-09-22 16:49:57 +0300

    Pridetas biblioteku failas

---
+++ Mylib.h
@@ -0,0 +1,16 @@
+#ifndef MYLIB_H_INCLUDED
+#define MYLIB_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <iomanip>
+#include <limits>
+using namespace std;
+
+
+#endif // MYLIB_H_INCLUDED
