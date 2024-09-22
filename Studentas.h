eea1e0fb2e40be0fd20a2587fc7886b59ade973d
Darj0 <baranovadar04@gmail.com>
2024-09-22 16:51:11 +0300

    Ikelti failai su funkcijomis ir struktura

---
+++ Studentas.h
@@ -0,0 +1,26 @@
+#ifndef STUDENTAS_H_INCLUDED
+#define STUDENTAS_H_INCLUDED
+#include "Mylib.h"
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egz;
+    double galutinis = 0.0;
+    string reikalavimas;
+};
+
+void galutinis_balas_vid(vector<Studentas>& studentai);
+void galutinis_balas_med(vector<Studentas>& studentai);
+void print_results(const vector<Studentas>& studentai);
+void patikrinimas(vector<Studentas>& studentai);
+void skaityti(vector<Studentas>& studentai, string name);
+void duomenys(vector<Studentas>& studentai);
+void sort_students(vector<Studentas>& studentai);
+bool compare_students(const Studentas& a, const Studentas& b);
+string capitalize(string var);
+string tolowers(string var);
+
+
+#endif // STUDENTAS_H_INCLUDED
