fd1e24df9559dfc0db8c5cca1967aebf69f2a8c1
Darj0 <baranovadar04@gmail.com>
2024-09-22 16:35:08 +0300

    main'as patobulintas, yra pasirinkimo galimybe
    Galima pasirinkti ar norime nuskaituti is failo, ar ivesti duomenis ranka

--- main.cpp
+++ main.cpp
@@ -30,10 +30,44 @@ string tolowers(string var);
 
 int main() {
     vector<Studentas> studentai;
+    int pasirinkimas;
+    string name;
+    while (true) {
+        cout << "Norite ivesti studentus(spauskyte 1) ar nuskaityti is failo(spauskyte 2)?\n";
+        cin >> pasirinkimas;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            cout << "Klaidingas ivedimas, bandykite dar karta (Iveskite tik skaiciu): \n";
+        }
+        else if (pasirinkimas != 1 && pasirinkimas != 2) {
+                            cout << "Klaida! Iveskite 1 arba 2: \n";
+        }
+
+        else {
+            break;
+        }
+    }
 
 
-    duomenys(studentai);
-    print_results(studentai);
+    if (pasirinkimas == 1)
+    {
+        duomenys(studentai);
+        print_results(studentai);
+    }
+
+    else
+    {
+        cout<<"Iveskyte failo kelia\n";
+        cin >> name;
+        skaityti(studentai, name);
+        print_results(studentai);
+       //patikrinimas(studentai);
+
+    }
+
+
+
 
 
     return 0;
