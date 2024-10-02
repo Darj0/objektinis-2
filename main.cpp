1a61cb2168be7f440a03308939435507d281e6ca
Darj0 <baranovadar04@gmail.com>
2024-10-02 10:53:26 +0300

    pakeistas main pridetas naujas pasirinkimas
    galima pasirinkti "generuoti studentu sarasu failus"

--- main.cpp
+++ main.cpp
@@ -9,14 +9,14 @@ int main() {
     int pasirinkimas;
     string name;
     while (true) {
-        cout << "Norite ivesti studentus(spauskyte 1) ar nuskaityti is failo(spauskyte 2)?\n";
+        cout << "Norite ivesti studentus(spauskite 1), nuskaityti is failo(spauskite 2) ar generuoti studentu sarasus (spauskite 3)?\n";
         cin >> pasirinkimas;
         if (cin.fail()) {
             cin.clear();
             cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
             cout << "Klaidingas ivedimas, bandykite dar karta (Iveskite tik skaiciu): \n";
         }
-        else if (pasirinkimas != 1 && pasirinkimas != 2) {
+        else if (pasirinkimas != 1 && pasirinkimas != 2 && pasirinkimas != 3) {
                             cout << "Klaida! Iveskite 1 arba 2: \n";
         }
 
@@ -33,7 +33,7 @@ int main() {
         print_results(studentai);
     }
 
-    else
+    else if (pasirinkimas == 2)
     {
         cout<<"Iveskyte failo kelia\n";
         cin >> name;
@@ -41,6 +41,19 @@ int main() {
         sort_students(studentai);
         print_results(studentai);
        //patikrinimas(studentai);
+
+    }
+
+    else
+    {
+        int n = 5;
+        generuoti_sarasus(n, studentai);
+        cout << "Sugeneruoti 5 failai: \n";
+        cout << "studentai_1000.txt \n";
+        cout << "studentai_10000.txt \n";
+        cout << "studentai_100000.txt \n";
+        cout << "studentai_1000000.txt \n";
+        cout << "studentai_10000000.txt \n";
 
     }
 
