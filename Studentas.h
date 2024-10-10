39c4c799b82e53201cdf9360fdaf7cb5015bffb8
Darj0 <baranovadar04@gmail.com>
2024-10-10 18:58:05 +0300

    Update Studentas.h

--- Studentas.h
+++ Studentas.h
@@ -15,7 +15,7 @@ void galutinis_balas_vid(vector<Studentas>& studentai);
 void galutinis_balas_med(vector<Studentas>& studentai);
 void print_results(const vector<Studentas>& studentai);
 void patikrinimas(vector<Studentas>& studentai);
-void skaityti(vector<Studentas>& studentai, string name);
+void skaityti(vector<Studentas>& studentai, string name, string kriterijus);
 void duomenys(vector<Studentas>& studentai);
 void sort_students(vector<Studentas>& studentai);
 bool compare_students(const Studentas& a, const Studentas& b);
