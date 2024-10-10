2aefb3e3cc159faa79e48c27d715bfb0fe7872c9
Darj0 <baranovadar04@gmail.com>
2024-10-10 18:59:22 +0300

    Update Failai.h

--- Failai.h
+++ Failai.h
@@ -7,12 +7,14 @@ string generuoti_pavarde(int indeksas);
 void kurti_faila(vector<Studentas>& studentai, string failo_priedas);
 void generuoti_sarasus(int n, vector<Studentas>& studentai);
 void rusiavimas_2_grupes(const vector<Studentas>& studentai, vector<Studentas>& vargsiukai, vector<Studentas>& kietiakiai);
-void darbas_su_failais(string failas, int duom_sk, string reikalavimas);
+void darbas_su_failais(string failas, int duom_sk, string reikalavimas, string kriterijus);
 void surusioti_failai(vector<Studentas>& studentai, string name);
 void sort_students_by_surname(vector<Studentas>& studentai);
 bool compare_by_surname(const Studentas& a, const Studentas& b);
 void sort_students_by_name(vector<Studentas>& studentai);
 bool compare_by_name(const Studentas& a, const Studentas& b);
+void sort_students_by_grade(vector<Studentas>& studentai);
+bool compare_by_grade(const Studentas& a, const Studentas& b);
 
 
 
