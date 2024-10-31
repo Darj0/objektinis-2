ab0f43ac9e8b8f52932371018560a5c4302db0a5
Darj0 <baranovadar04@gmail.com>
2024-10-31 11:51:06 +0200

    Update Failai.h

--- Failai.h
+++ Failai.h
@@ -6,8 +6,8 @@ string generuoti_varda(int indeksas);
 string generuoti_pavarde(int indeksas);
 void kurti_faila(vector<Studentas>& studentai, string failo_priedas);
 void generuoti_sarasus(int n, vector<Studentas>& studentai);
-void rusiavimas_2_grupes(const vector<Studentas>& studentai, vector<Studentas>& vargsiukai, vector<Studentas>& kietiakiai);
-void darbas_su_failais(string failas, int duom_sk, string reikalavimas, string kriterijus);
+void rusiavimas_2_grupes(const vector<Studentas>& studentai, vector<Studentas>& vargsiukai, vector<Studentas>& kietiakiai, int strategija);
+void darbas_su_failais(string failas, int duom_sk, string reikalavimas, string kriterijus, int strategija);
 void surusioti_failai(vector<Studentas>& studentai, string name);
 void sort_students_by_surname(vector<Studentas>& studentai);
 bool compare_by_surname(const Studentas& a, const Studentas& b);
@@ -15,12 +15,14 @@ void sort_students_by_name(vector<Studentas>& studentai);
 bool compare_by_name(const Studentas& a, const Studentas& b);
 void sort_students_by_grade(vector<Studentas>& studentai);
 bool compare_by_grade(const Studentas& a, const Studentas& b);
+void print_memory_usage(const vector<Studentas>& v, const string& name);
 
-void rusiavimas_2_grupes_list(const list<Studentas>& studentai_list, list<Studentas>& vargsiukai_list, list<Studentas>& kietiakiai_list);
-void darbas_su_failais_list(string failas, int duom_sk, string reikalavimas, string kriterijus);
+void rusiavimas_2_grupes_list(const list<Studentas>& studentai_list, list<Studentas>& vargsiukai_list, list<Studentas>& kietiakiai_list, int strategija);
+void darbas_su_failais_list(string failas, int duom_sk, string reikalavimas, string kriterijus, int strategija);
 void surusioti_failai_list(list<Studentas>& studentai_list, const string& name);
 void sort_students_by_name_list(list<Studentas>& studentai_list);
 void sort_students_by_surname_list(list<Studentas>& studentai_list);
 void sort_students_by_grade_list(list<Studentas>& studentai_list);
+void print_memory_usage_list(const vector<Studentas>& v, const string& name);
 
 #endif // FAILAI_H_INCLUDED
